f5e48802f86dc9261ef48e9d01ecf4051d9e2cfd
로헨 <shehdgus97@naver.com>
2022-05-22 16:23:27 +0900

    장애물 움직임 구현
    saw 좌우 움직임 구현

--- Assets/Script/SystemScript/SawControl.cs
+++ Assets/Script/SystemScript/SawControl.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class SawControl : MonoBehaviour
 {
     private Transform trans;
+    private float posX;
+    public float minPos;
+    public float maxPos;
+    private bool triger = true;
     // Start is called before the first frame update
     void Start()
     {
         trans = GetComponent<Transform>();
+        posX = transform.position.x;
+
+        minPos = posX - minPos;
+        maxPos = posX + maxPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        trans.Rotate(new Vector3(0, 0, 100.0f * Time.deltaTime));
+        //trans.Rotate(new Vector3(0, 0, 100.0f * Time.deltaTime));
+
+        if (triger)
+        {
+            transform.Translate(Vector2.right * 1.0f*Time.deltaTime);
+            if(transform.position.x >= maxPos)
+            {
+                triger = !triger;
+            }
+        }
+        else if (!triger)
+        {
+            transform.Translate(Vector2.left * 1.0f * Time.deltaTime);
+            if (transform.position.x <= minPos)
+            {
+                triger = !triger;
+            }
+        }
     }
 }
